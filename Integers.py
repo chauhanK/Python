d4e43e35578b3d4bc93701a37e709714727cbd72
Karan Chauhan <kchauhan@mycom-internal.com>
2018-06-19 10:52:05 +0530

    Integer and Floats

---
+++ Integers.py
@@ -0,0 +1,33 @@
+
+num = 3 
+
+print(type(num))
+
+#===========Arithmetic Operators=================
+
+print(3 + 2)
+
+#Floor Division
+print(3 // 2)
+
+#Exponent
+print(3 ** 2)
+
+#Modulus
+print(3 % 2)
+
+#Absolute value
+print(abs(-3))
+
+#Round value
+print(round(3.75))
+
+print(round(3.75,1))
+
+num_1 ='100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
